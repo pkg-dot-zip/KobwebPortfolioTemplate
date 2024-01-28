a3ef94235ccb28fb48e962b93888f72af484ec66
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-01-28 12:26:15 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform) apply false
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+    }
+}
---
+++ site/build.gradle.kts
@@ -0,0 +1,38 @@
+import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+    alias(libs.plugins.kobwebx.markdown)
+}
+
+group = "io.github.username.projectname"
+version = "1.0-SNAPSHOT"
+
+kobweb {
+    app {
+        index {
+            description.set("Powered by Kobweb")
+        }
+    }
+}
+
+kotlin {
+    configAsKobwebApplication("projectname")
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+        }
+
+        jsMain.dependencies {
+            implementation(compose.html.core)
+            implementation(libs.kobweb.core)
+            implementation(libs.kobweb.silk)
+            implementation(libs.silk.icons.fa)
+            implementation(libs.kobwebx.markdown)
+            
+        }
+    }
+}
