1655f28d216db6a73224b3ebfec221fa35a5dac1
pkg-dot-zip <simon@pkg-dot-zip.com>
2024-02-07 14:59:55 +0100

    Stylized HorizontalRuler

--- site/src/jsMain/kotlin/io/github/username/projectname/components/widgets/HorizontalRuler.kt
+++ site/src/jsMain/kotlin/io/github/username/projectname/components/widgets/HorizontalRuler.kt
@@ -5,6 +5,7 @@ import com.varabyte.kobweb.compose.css.CSSLengthNumericValue
 import com.varabyte.kobweb.compose.css.CSSLengthOrPercentageNumericValue
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.border
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.margin
 import com.varabyte.kobweb.compose.ui.toAttrs
@@ -15,10 +16,10 @@ import org.jetbrains.compose.web.dom.Hr
 
 @Composable
 fun HorizontalRuler(
-    fillAmount: CSSLengthOrPercentageNumericValue = 90.percent,
+    fillAmount: CSSLengthOrPercentageNumericValue = (100F / 1.2F).percent,
     marginTopBottom: CSSLengthNumericValue = 2.cssRem,
-    borderWidth: CSSLengthNumericValue = 1.px,
-    borderStyle: LineStyle = LineStyle.Dotted,
+    borderWidth: CSSLengthNumericValue = 4.px,
+    borderStyle: LineStyle = LineStyle.Solid,
     borderColor: CSSColorValue = when (ColorMode.current) {
         ColorMode.LIGHT -> Res.Color.HORIZONTAL_RULER_COLOR_LIGHT
         ColorMode.DARK -> Res.Color.HORIZONTAL_RULER_COLOR_DARK
@@ -26,6 +27,6 @@ fun HorizontalRuler(
 ) {
     Hr(
         Modifier.fillMaxWidth(fillAmount).margin(topBottom = marginTopBottom)
-            .border(width = borderWidth, style = borderStyle, color = borderColor).toAttrs()
+            .border(width = borderWidth, style = borderStyle, color = borderColor).borderRadius(7.px).toAttrs()
     )
 }
